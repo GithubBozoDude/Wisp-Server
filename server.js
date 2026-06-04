6bc91fc8e635772f436a72137f2ece7bac7aad5a
GithubBozoDude <164231874+GithubBozoDude@users.noreply.github.com>
2026-06-04 13:22:03 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
+import http from "http";
+import { server as wisp } from "@mercuryworkshop/wisp-js/server";
 
+const httpServer = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end("wisp server");
+});
+
+httpServer.on("upgrade", (req, socket, head) => {
+  wisp.routeRequest(req, socket, head);
+});
+
+const PORT = process.env.PORT || 8080;
+httpServer.listen(PORT, "0.0.0.0", () => {
+  console.log(`Wisp server running on port ${PORT}`);
+});
